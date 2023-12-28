7f95e97213b48c660db13efb909d538920e9ef57
deepak ab <ab7087670@gmail.com>
2023-12-28 21:09:56 +0800

    commit

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import netflix from "../Assets/Netflix_Logo_RGB.png";
 
 function Navbar(props) {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Navbar</Link>
+    <Link className="navbar-brand" to="/"><img src={netflix} alt=""  width={150} height={80}/></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
